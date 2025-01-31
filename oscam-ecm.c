e37503606cd5167b067d0982f25b7d7475b98f10
hehe1323 <chirongda@hotmail.com>
2025-01-31 17:30:20 +0800

    Update oscam-ecm.c

--- oscam-ecm.c
+++ oscam-ecm.c
@@ -204,6 +204,18 @@ static void streamguard_xor_cw(uint8_t *cw, uint8_t *key) {
 }
 #endif
 
+// 修改oscam-ecm.c的ecm处理入口：
+int process_ecm(struct s_client *client, ECM_REQUEST *er) {
+   #ifdef STREAMGUARD_ENABLED
+   if (er->ecm[0] == 0x81 && er->ecm[1] == 0x70) {
+       return streamguard_process_ecm(client, er);
+   }
+   #endif
+    // 原有处理流程...
+}
+
+
+	
 uint8_t checkCWpart(uint8_t *cw, int8_t part)
 {
 	uint8_t eo = part ? 8 : 0;
